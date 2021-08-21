a651f5fa3129eb68f135ecd7bb0b91220bf7366f
Karlo <49595438+Karl255@users.noreply.github.com>
2021-08-21 16:14:57 +0200

    Initial commit

---
+++ NBodySim/NBodySimGame.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace NBodySim
+{
+	public class NBodySimGame : Game
+	{
+		private GraphicsDeviceManager Graphics;
+		private SpriteBatch SpriteBatch;
+
+		public NBodySimGame()
+		{
+			Graphics = new GraphicsDeviceManager(this);
+			Content.RootDirectory = "Content";
+			IsMouseVisible = true;
+		}
+
+		protected override void Initialize()
+		{
+			base.Initialize();
+		}
+
+		protected override void LoadContent()
+		{
+			SpriteBatch = new SpriteBatch(GraphicsDevice);
+		}
+
+		protected override void Update(GameTime gameTime)
+		{
+			base.Update(gameTime);
+		}
+
+		protected override void Draw(GameTime gameTime)
+		{
+			GraphicsDevice.Clear(Color.Black);
+
+			base.Draw(gameTime);
+		}
+	}
+}
