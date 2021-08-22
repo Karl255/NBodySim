34197666e25c83f115e33bf36ab936db46876e37
Karlo <49595438+Karl255@users.noreply.github.com>
2021-08-22 15:04:07 +0200

    Added ability to remove all spawned bodies + slight refactoring

--- NBodySim/NBodySimGame.cs
+++ NBodySim/NBodySimGame.cs
@@ -20,7 +20,7 @@ namespace NBodySim
 		private int Scale = 2;
 		private List<Body> Bodies;
 
-		private bool AllowCollisions = false;
+		private bool AllowCollisions = true;
 		private int RadiusInput = 10; // +-1
 		private int MassInput = 1000; // +-10
 		private float VelocityXInput = 0; // +-0.1f
@@ -72,6 +72,7 @@ namespace NBodySim
 			var mouseState = Mouse.GetState();
 			var keyboardState = Keyboard.GetState();
 
+			// left click
 			if (mouseState.LeftButton == ButtonState.Pressed && PreviousMouseState.LeftButton == ButtonState.Released)
 			{
 				Bodies.Add(new(
@@ -82,33 +83,47 @@ namespace NBodySim
 					new Vector2(VelocityXInput, VelocityYInput)));
 			}
 
+			// right click
 			if (mouseState.RightButton == ButtonState.Pressed && PreviousMouseState.RightButton == ButtonState.Pressed)
 				Origin += (PreviousMouseState.Position - mouseState.Position).ToVector2() * Scale;
 
+			// scroll
 			if (mouseState.ScrollWheelValue != PreviousMouseState.ScrollWheelValue)
 			{
 				int prevScale = Scale;
 				Scale = MathHelper.Max(1, Scale + (PreviousMouseState.ScrollWheelValue - mouseState.ScrollWheelValue) / 120);
 			}
 
+			// C
 			if (keyboardState.IsKeyDown(Keys.C) && PreviousKeyboardState.IsKeyUp(Keys.C))
 				AllowCollisions = !AllowCollisions;
 
+			// R
+			if (keyboardState.IsKeyDown(Keys.R) && PreviousKeyboardState.IsKeyUp(Keys.R))
+			{
+				Bodies.Clear();
+				Origin = new(0, 0);
+			}
+
+			// +/-
 			if (keyboardState.IsKeyDown(Keys.Add))
 				RadiusInput += 1;
 			if (keyboardState.IsKeyDown(Keys.Subtract))
 				RadiusInput = Math.Max(1, RadiusInput - 1);
 
+			// page up/page down
 			if (keyboardState.IsKeyDown(Keys.PageUp))
 				MassInput += 10;
 			if (keyboardState.IsKeyDown(Keys.PageDown))
 				MassInput = Math.Max(10, MassInput - 10);
 
+			// right/left
 			if (keyboardState.IsKeyDown(Keys.Right))
 				VelocityXInput += 0.1f;
 			if (keyboardState.IsKeyDown(Keys.Left))
 				VelocityXInput -= 0.1f;
 			
+			// up/down
 			if (keyboardState.IsKeyDown(Keys.Up))
 				VelocityYInput -= 0.1f;
 			if (keyboardState.IsKeyDown(Keys.Down))
@@ -119,6 +134,7 @@ namespace NBodySim
 
 			if (AllowCollisions)
 				Bodies = Body.SimulateCollisions(Bodies);
+
 			Body.SimulateGravity(Bodies);
 			for (int i = 0; i < Bodies.Count; i++)
 				Bodies[i].Update();
@@ -135,13 +151,15 @@ namespace NBodySim
 			for (int i = 0; i < Bodies.Count; i++)
 				Bodies[i].Draw(SpriteBatch, ScreenSize, Origin, Scale);
 
-			SpriteBatch.DrawString(UIFont, $"Position: ({Origin.X}, {Origin.Y})", new(0, 0 * UIFont.LineSpacing), Color.White);
-
-			SpriteBatch.DrawString(UIFont, "Input values:",                                   new(0, 2 * UIFont.LineSpacing), Color.White);
-			SpriteBatch.DrawString(UIFont, $"Radius: {RadiusInput}",                          new(0, 3 * UIFont.LineSpacing), Color.White);
-			SpriteBatch.DrawString(UIFont, $"Mass: {MassInput}",                              new(0, 4 * UIFont.LineSpacing), Color.White);
-			SpriteBatch.DrawString(UIFont, $"Velocity: ({VelocityXInput}, {VelocityYInput})", new(0, 5 * UIFont.LineSpacing), Color.White);
-			SpriteBatch.DrawString(UIFont, $"Collisions: {(AllowCollisions ? "on" : "off")}", new(0, 6 * UIFont.LineSpacing), Color.White);
+			int row = 0;
+			SpriteBatch.DrawString(UIFont, $"Position: ({Origin.X}, {Origin.Y})", new(0, row++ * UIFont.LineSpacing), Color.White);
+			SpriteBatch.DrawString(UIFont, $"Body count: {Bodies.Count}",         new(0, row++ * UIFont.LineSpacing), Color.White);
+			row++;
+			SpriteBatch.DrawString(UIFont, "Input values:",                                   new(0, row++ * UIFont.LineSpacing), Color.White);
+			SpriteBatch.DrawString(UIFont, $"Radius: {RadiusInput}",                          new(0, row++ * UIFont.LineSpacing), Color.White);
+			SpriteBatch.DrawString(UIFont, $"Mass: {MassInput}",                              new(0, row++ * UIFont.LineSpacing), Color.White);
+			SpriteBatch.DrawString(UIFont, $"Velocity: ({VelocityXInput}, {VelocityYInput})", new(0, row++ * UIFont.LineSpacing), Color.White);
+			SpriteBatch.DrawString(UIFont, $"Collisions: {(AllowCollisions ? "on" : "off")}", new(0, row++ * UIFont.LineSpacing), Color.White);
 
 			SpriteBatch.End();
 
